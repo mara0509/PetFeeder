a0314df438c62da05876c1f8256e26c825994063
mara0509 <tamasilamara@gmail.com>
2026-06-01 19:40:40 +0300

    chnages

--- cloud/app.py
+++ cloud/app.py
@@ -4,9 +4,7 @@ import datetime
 
 app = Flask(__name__)
 
-# ==========================================
-# FIREBASE CONFIGURATION
-# ==========================================
+
 firebaseConfig = {
   "apiKey": "AIzaSyDMLnwwDZdFTaD-9KsOnZtUQaQjA9EWziI",
   "authDomain": "petfeeder-d4b6d.firebaseapp.com",
@@ -23,19 +21,15 @@ db = firebase.database()
 @app.route('/')
 def index():
     try:
-        # 1. Get the Live Status
         live_data = db.child("feeder_status").child("live").get().val()
-        
-        # 2. Get the latest 10 History logs
+    
         history_node = db.child("feeder_status").child("history").order_by_key().limit_to_last(10).get().val()
         
-        # Convert history dictionary to a list and reverse it so newest is on top
         history_list = []
         if history_node:
             for key, value in history_node.items():
                 history_list.append(value)
             history_list.reverse()
-	# 3. Get Schedule
         schedule_node = db.child("feeder_status").child("schedule").get().val()
         schedule_list = [v for k, v in schedule_node.items()] if schedule_node else []
             
@@ -44,34 +38,27 @@ def index():
         live_data = None
         history_list = []
 
-    # Send data to the HTML page
     return render_template('index.html', live=live_data, history=history_list, schedule=schedule_list)
 
 @app.route('/dispense', methods=['POST'])
 def dispense():
     try:
-        # Create a command payload with a timestamp
         command_data = {
             "action": "dispense",
             "trigger": True,
             "timestamp": str(datetime.datetime.now())
         }
-        # Write it to a new "commands" node in Firebase
         db.child("commands").set(command_data)
         print("[WEB] Dispense command sent to Firebase!")
     except Exception as e:
         print(f"Error sending command: {e}")
-
-    # Refresh the page immediately
     return redirect(url_for('index'))
 
 @app.route('/set_schedule', methods=['POST'])
 def set_schedule():
     try:
-        # Get the time from the HTML form (Format will be "HH:MM")
         feed_time = request.form.get('feed_time')
         if feed_time:
-            # Push the new time to Firebase
             db.child("feeder_status").child("schedule").push(feed_time)
             print(f"[WEB] Scheduled new feeding time: {feed_time}")
     except Exception as e:
